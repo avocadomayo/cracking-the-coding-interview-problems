fbf637731df71b4bd902a115fa5db0ebe6ce89b8
Wendy Liu <wendy_jy_liu@hotmail.com>
2018-03-30 14:44:07 -0700

    Project setup

---
+++ src/main/java/ca/wendyliu/cracking/the/coding/interview/problems/chapter1/Problem5.java
@@ -0,0 +1,8 @@
+package ca.wendyliu.cracking.the.coding.interview.problems.chapter1;
+
+public class Problem5 {
+
+    public static boolean checkOneAway(String a, String b) {
+        return false;
+    }
+}
---
+++ src/test/java/ca/wendyliu/cracking/the/coding/interview/problems/chapter1/Problem5Test.java
@@ -0,0 +1,13 @@
+package ca.wendyliu.cracking.the.coding.interview.problems.chapter1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Problem5Test {
+
+    @Test
+    public void test() {
+        assertTrue(Problem5.checkOneAway("aple", "apple"));
+    }
+}
